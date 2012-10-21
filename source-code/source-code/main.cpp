fdda6bdf39efbfbbfc99fa5171de213b2c76b96d
Guna Prasaad <gunaprsd@gmail.com>
2012-10-21 19:49:28 +0530

    created xcode project

---
+++ source-code/source-code/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  source-code
+//
+//  Created by Guna Prasaad on 21/10/12.
+//  Copyright (c) 2012 Guna Prasaad. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
