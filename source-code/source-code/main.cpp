396e419185c19784036ffb15f2fff8868528366f
manojtld <kakerrott94@gmail.com>
2012-10-29 14:40:35 +0530

    Update source-code/source-code/main.cpp

--- source-code/source-code/main.cpp
+++ source-code/source-code/main.cpp
@@ -9,6 +9,7 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include "LexicalWordFeatures.cpp"
 using namespace std;
 
 
@@ -18,12 +19,14 @@ using namespace std;
 
 int main(int argc, const char * argv[])
 {
+  
 
-   string word;
     ifstream file;
+    
 	int letters=0;
 	int characters = 0;
     int sentences = 0;
+    int paragraphs = 1;
     int sentence_word_count = 0;
     int words = 0;
     int word_letter_count = 0;
@@ -34,14 +37,15 @@ int main(int argc, const char * argv[])
 	int spaces = 0;
 	int punctuation = 0;
 	int tabs = 0;
+    int lines = 1;	
 	int ascii;
+    string word = "";
     int token_count= 0;
     int sentence_count = 0; //note sentence does not mean a sentence, it is a clause
     float average_letter_count = 0.0;
     float average_word_count = 0.0;
+    LexicalWordFeatures W;
 
-	
-	
 	file.open("Myfile.txt"); 
 	if(!file)
 	{
@@ -51,9 +55,9 @@ int main(int argc, const char * argv[])
 	char ch;
 	while(file.get(ch))
 	{ 
-			
+
 			ascii = ch;
-			
+
         if(ch=='!' || ch == '?' || ch == '.')
             {
                 punctuation++;
@@ -65,43 +69,50 @@ int main(int argc, const char * argv[])
                 /*sentence metrics update*/
                 average_word_count = (average_word_count*sentence_count + sentence_word_count)/++sentence_count;
                 sentence_word_count = 0;
-            
+
+               W.addSentence(word);
+				word.clear();
             }
 			else if((ascii>64)&&(ascii<91))
 			{
 			    uppercase++; word_letter_count++;
+                            word += ch;
+				W.incrementCharacterCount();
 			}
-			
+
 			else if((ascii>96)&&(ascii<123))
 			{
 				lowercase++; word_letter_count++;
+                                  word += ch;
+				W.incrementCharacterCount();
 			}
-			
+
 			else if((ascii>=39)&&(ascii<=64))
 			{
-                characters++; 
-			}
-			
+                               characters++; 
+				word += ch;
+				W.incrementCharacterCount();
+	    		}
+
 			else if(isdigit(ch))
 			{
 				numbers++;
+				word += ch;
+				W.incrementCharacterCount();
 			}
+
 			
-			else if(isspace(ch))
-			{
-				spaces++;
-                average_letter_count = (average_letter_count*token_count + word_letter_count)/++token_count;
-                word_letter_count = 0;
-                sentence_word_count++;
-			}
-			
-            else if(ispunct(ch))
+                   else if(ispunct(ch))
 			{
 				punctuation++;
                 
                 /*word metrics update*/
                 average_letter_count = (average_letter_count*token_count + word_letter_count)/++token_count;
                 word_letter_count = 0;
+                          
+				W.incrementCharacterCount();
+				word += ch;
+              
             }
 
 			else if(ch == '\t')
@@ -110,8 +121,34 @@ int main(int argc, const char * argv[])
                 average_letter_count = (average_letter_count*token_count + word_letter_count)/++token_count;
                 word_letter_count = 0;
                 ++sentence_word_count;
-                
+		W.addSentence(word);
+		word.clear();
+		paragraphs++;
+		sentences++;
+		
 			}
+		
+		else if(ch == '\n') {
+			paragraphs++;
+			sentences++;
+		W.addSentence(word);
+		word.clear();
+		lines++;
+		}
+		
+		else if(isspace(ch))
+			{
+				spaces++;
+                average_letter_count = (average_letter_count*token_count + word_letter_count)/++token_count;
+                word_letter_count = 0;
+                sentence_word_count++;
+
+                                W.addWord(word);
+				W.incrementCharacterCount();
+				word.clear();
+			}
+
+                
             else if (ch == '.') {
                 sentences++;
                 average_letter_count = (average_letter_count*token_count + word_letter_count)/++token_count;
@@ -119,13 +156,20 @@ int main(int argc, const char * argv[])
                 sentence_word_count++;
                 average_word_count = (average_word_count*sentence_count + sentence_word_count)/++sentence_count;
                 sentence_word_count = 0;
+                
+               
+		W.incrementCharacterCount();
+		W.addSentence(word);
+		word.clear();
             }
-			
+
             else
             {
             	specialchars++;
+		W.incrementCharacterCount();
+		word += ch;
             }
-			
+
 	}
 	letters = uppercase+lowercase;
 	cout << "Total number of letters \t" << letters << endl;
@@ -139,9 +183,21 @@ int main(int argc, const char * argv[])
     cout << "total number of sentences "<<sentence_count<<endl;
     cout << "average words in a sentence" << average_word_count<<endl;
     cout << "average letters in a word"<<average_letter_count<<endl;
+    cout << "total number of paragraphs "<< paragraphs<<endl;
+    cout << "total number of lines " <<lines<<endl;
+    cout << "average number of lines per paragraph "<< lines/paragraphs <<endl;
+    cout << "average number of characters per paragraph "<< letters/paragraphs <<endl;
+    cout << "average number of words per paragraph "<< token_count/paragraphs <<endl;
+    cout << "average number of sentences per paragraph "<< sentence_count/paragraphs <<endl;
+    cout << "hapex legomena of the text is-> " ;
+	 W.hapexlegomena();
+	 cout<<endl<<endl<<endl;
+    cout << "hapex dislegomena of the text is->" <<" ";
+	W.hapexdislegomena();
+	cout<<endl;
+    cout << "yules k measure of the text is ";
+    W.yulesmeasure();
     
-	
+
 	file.close();
     return 0;
-}
-
